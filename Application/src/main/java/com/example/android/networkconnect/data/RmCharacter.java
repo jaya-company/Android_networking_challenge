bf12a6aaed6be806c13b2da454f679d5036f7235
Rafael Peñates <andres.penates@gmail.com>
2020-12-29 06:54:34 -0500

    Add base config for retrieve api

---
+++ Application/src/main/java/com/example/android/networkconnect/data/RmCharacter.java
@@ -0,0 +1,23 @@
+package com.example.android.networkconnect.data;
+
+public class RmCharacter {
+
+    private String name;
+    private String imageUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
